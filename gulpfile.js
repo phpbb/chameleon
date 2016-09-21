26f214d641107b804963ca2e3fe14f0f2b975711
Michael Miday <midaym@gmail.com>
2016-09-21 15:47:54 +0200

    use xo

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
 var sass = require('gulp-sass');
@@ -10,69 +12,67 @@ var basel = require('./package.json');
 
 // Config
 var build = {
-    css: './dist/assets/css',
-    scss: './src/scss/'
+	css: './dist/assets/css',
+	scss: './src/scss/'
 };
 
 var minify = true;
 
 var AUTOPREFIXER_BROWSERS = [
-    'ie >= 11',
-    'edge >= 20',
-    'ff >= 40',
-    'chrome >= 35',
-    'safari >= 8',
-    'opera >= 35',
-    'ios >= 8'
+	'ie >= 11',
+	'edge >= 20',
+	'ff >= 40',
+	'chrome >= 35',
+	'safari >= 8',
+	'opera >= 35',
+	'ios >= 8'
 ];
 
 var render = function (layer) {
-    'use strict';
+	'use strict';
 
-    var css = gulp
-        .src(build.scss + layer + '.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass({
-            precision: 10,
-            onError: console.error.bind(console, 'Sass error:')
-        }))
-        .pipe(autoprefixer(AUTOPREFIXER_BROWSERS));
+	var css = gulp
+	.src(build.scss + layer + '.scss')
+	.pipe(sourcemaps.init())
+	.pipe(sass({
+		precision: 10,
+		onError: console.error.bind(console, 'Sass error:')
+	}))
+	.pipe(autoprefixer(AUTOPREFIXER_BROWSERS));
 
-    if (minify) {
-        css = css
-            .pipe(csso())
-            .pipe(rename({
-                suffix: '.' + basel.version + '.min',
-                extname: '.css'
-            }));
-    } else {
-        css = css
-            .pipe(csscomb());
-    }
+	if (minify) {
+		css = css
+		.pipe(csso())
+		.pipe(rename({
+			suffix: '.' + basel.version + '.min',
+			extname: '.css'
+		}));
+	} else {
+		css = css
+		.pipe(csscomb());
+	}
 
-    css = css
-        .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest(build.css));
+	css = css
+	.pipe(sourcemaps.write('./'))
+	.pipe(gulp.dest(build.css));
 
-    return css;
+	return css;
 };
 
 gulp.task('core', function () {
-    render('core');
+	render('core');
 });
 
 gulp.task('utilities', function () {
-    render('utilities');
+	render('utilities');
 });
 
 gulp.task('clean', function () {
-    'use strict';
-    del(['dist']);
+	del(['dist']);
 });
 
 gulp.task('watch', function () {
-    'use strict';
-    gulp.watch('src/scss/*.scss', ['css', 'utilities']);
+	gulp.watch('src/scss/*.scss', ['css', 'utilities']);
 });
 
 gulp.task('serve', ['watch']);
