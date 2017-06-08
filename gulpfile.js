d72f66ac072d36a7ab80b5c80385b1b46858c1ae
Michael Miday <midaym@gmail.com>
2017-06-08 17:42:01 -0400

    ecma things up

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,27 @@
 'use strict';
 
-var del = require('del');
-var gulp = require('gulp');
-var autoprefixer = require('gulp-autoprefixer');
-var sass = require('gulp-sass');
-var rename = require('gulp-rename');
-var sourcemaps = require('gulp-sourcemaps');
-var cssnano = require('gulp-cssnano');
-var postcss = require('gulp-postcss');
-var stylefmt = require('gulp-stylefmt');
-var sorting = require('postcss-sorting');
-var torem = require('postcss-pxtorem');
-var sortOrder = require('./.postcss-sorting.json');
-var pkg = require('./package.json');
+const del = require('del');
+const gulp = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
+const sass = require('gulp-sass');
+const rename = require('gulp-rename');
+const sourcemaps = require('gulp-sourcemaps');
+const cssnano = require('gulp-cssnano');
+const postcss = require('gulp-postcss');
+const stylefmt = require('gulp-stylefmt');
+const sorting = require('postcss-sorting');
+const torem = require('postcss-pxtorem');
+const sortOrder = require('./.postcss-sorting.json');
+const pkg = require('./package.json');
 
 // Config
-var build = {
+const build = {
 	css: './dist/assets/css',
 	docs: './docs/_media/',
 	scss: './src/scss/'
 };
 
-var AUTOPREFIXER_BROWSERS = [
+const AUTOPREFIXER_BROWSERS = [
 	'ie >= 11',
 	'edge >= 12',
 	'ff >= 38',
@@ -31,8 +31,8 @@ var AUTOPREFIXER_BROWSERS = [
 	'ios >= 8'
 ];
 
-gulp.task('css', function () {
-	var css = gulp
+gulp.task('css', () => {
+	const css = gulp
 	.src(build.scss + '*.scss')
 	.pipe(sourcemaps.init())
 	.pipe(sass({
@@ -80,12 +80,12 @@ gulp.task('css', function () {
 	return css;
 });
 
-gulp.task('clean', function () {
+gulp.task('clean', () => {
 	del(['dist']);
 });
 
-gulp.task('minify', ['css'], function () {
-	var css = gulp
+gulp.task('minify', ['css'], () => {
+	const css = gulp
 	.src(build.css + '/*.' + pkg.version + '.css')
 	.pipe(sourcemaps.init())
 	.pipe(cssnano())
@@ -99,8 +99,8 @@ gulp.task('minify', ['css'], function () {
 	return css;
 });
 
-gulp.task('docs:css', function () {
-	var css = gulp
+gulp.task('docs:css', () => {
+	const css = gulp
 	.src(build.docs + '*.scss')
 	.pipe(sourcemaps.init())
 	.pipe(sass({
@@ -149,31 +149,31 @@ gulp.task('docs:css', function () {
 	return css;
 });
 
-gulp.task('setup:settings', function () {
-	var settings = gulp
+gulp.task('setup:settings', () => {
+	const settings = gulp
 	.src('node_modules/base-l.settings/*.scss')
 	.pipe(gulp.dest('src/scss/settings/'));
 
 	return settings;
 });
 
-gulp.task('setup:mixins', function () {
-	var mixins = gulp
+gulp.task('setup:mixins', () => {
+	const mixins = gulp
 		.src('node_modules/base-l.tools/mixin/*.scss')
 		.pipe(gulp.dest('src/scss/tools/mixin/'));
 
 	return mixins;
 });
 
-gulp.task('setup:functions', function () {
-	var funcitons = gulp
+gulp.task('setup:functions', () => {
+	const funcitons = gulp
 		.src('node_modules/base-l.tools/function/*.scss')
 		.pipe(gulp.dest('src/scss/tools/function/'));
 
 	return funcitons;
 });
 
-gulp.task('watch', function () {
+gulp.task('watch', () => {
 	gulp.watch('src/scss/**/*.scss', ['css', 'minify']);
 	gulp.watch('docs/_media/*.scss', ['docs:css']);
 });
