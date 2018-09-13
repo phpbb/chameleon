e176f67c700a3c4a13e52ba2848d6d0339916d76
hanakin <midaym@gmail.com>
2018-09-13 15:58:35 +0200

    delete all

--- git-tools/setup-phpbb.sh
+++
@@ -1,5 +0,0 @@
-set -ex
-git remote add public-phpbb git@github.com:phpbb/phpbb.git
-git fetch public-phpbb
-git checkout -b phpbb/chameleon-core-changes origin/phpbb/chameleon-core-changes
-git checkout -b phpbb/develop origin/phpbb/develop
